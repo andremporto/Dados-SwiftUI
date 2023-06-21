08de9b5819a3b741c1a8cd9359664ba341a19016
André Porto <andreporto@me.com>
2023-06-21 09:40:13 -0300

    Initial Commit

---
+++ Dados-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dados-SwiftUI
+//
+//  Created by André Porto on 21/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dados-SwiftUI/Dados_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dados_SwiftUIApp.swift
+//  Dados-SwiftUI
+//
+//  Created by André Porto on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Dados_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
